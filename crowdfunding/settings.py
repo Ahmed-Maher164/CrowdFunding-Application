80ad136c2298141fcc9aae0d6a844b6867ade313
Ahmed Ibrahim <ahmedibrahem22322@gmail.com>
2021-03-30 00:21:21 +0200

    add users app

--- crowdfunding/settings.py
+++ crowdfunding/settings.py
@@ -75,12 +75,18 @@ WSGI_APPLICATION = 'crowdfunding.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': "django.db.backends.mysql",
         'NAME': 'crowdfunding',
-        'USER': 'maher',
-        'PASSWORD': 'maher123',
+        'USER': 'ibrahim',
+        'PASSWORD': 'ibrahim123',
         'HOST': '127.0.0.1',
     }
 }
