bdd0ef267fc46f7d15cd2b86d3a195a774d85e79
Mohamedelbeshlawy <mohamedelbeshlawyy@gmail.com>
2021-04-01 00:33:30 +0200

    add project-images/tags

--- projects/static/projects/js/main.js
+++ projects/static/projects/js/main.js
@@ -1,8 +1,21 @@
 let tagButton = document.getElementById("tagButton")
 let tagConatiner = document.getElementById("tagContainer")
+let photoButton = document.getElementById("photoButton")
+let photoConatiner = document.getElementById("photoContainer")
+let countImg = document.getElementById("countImg")
 
 function addTag() {
     let tag = document.createElement("input")
-    tag.setAttribute("name", "tag[]")
-//    tagConatiner.appendChild(tag)
+    tag.setAttribute("name", "tags[]")
+    tagConatiner.appendChild(tag)
+}
+
+let counter = -1
+function addPhoto() {
+    counter++
+    let photo = document.createElement("input")
+    photo.setAttribute("name", "photos"+counter)
+    photo.setAttribute("type", "file")
+    photoConatiner.appendChild(photo)
+    countImg.value = counter
 }
